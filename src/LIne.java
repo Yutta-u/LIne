8db87ac7f587c41b577fbee50eebe458551e890d
Yutta-u <davidchastoklovrf@gmail.com>
2022-08-30 18:23:28 +0500

    Initial commit

---
+++ src/LIne.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class LIne {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        double x = scan.nextInt();
+        double y = scan.nextInt();
+        double x1 = scan.nextInt();
+        double y1 = scan.nextInt();
+
+        double z = ((x - x1) * (x - x1)) + ((y - y1) * (y - y1));
+        double z1 = (double) Math.sqrt(z);
+        System.out.println(z1);
+    }
+}
